1a5e7eec04906fab8ed941d5caba185b91300b12
jshra <Jshutyra@gmail.com>
2024-11-13 16:58:49 +0100

    Fine-tune prompts, remove code markdown in outputs

--- htmlGenerator/htmlGenerator.py
+++ htmlGenerator/htmlGenerator.py
@@ -1,5 +1,5 @@
 from openai import OpenAI
-client = OpenAI(api_key = ':)')
+client = OpenAI(api_key = '')
 
 with open('text.txt', 'r', encoding='utf-8') as file:
     text = file.read()
@@ -18,32 +18,31 @@ htmlResponse = client.chat.completions.create(
                 "Return only the code to go between <body> and </body>"
                 "Exclude <html>, <head>, <body> tags from the response."
                 "Additionally, restore any missing Polish characters to the text. "
+                "Return the answer without code markdowns"
                 f"Here’s the text:{text}"
                 
             )}
     ]
 )
 cleanedHtml = htmlResponse.choices[0].message.content.strip()
-cleanedHtml = cleanedHtml[8:-3]
 
 imagePromptResponse = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[
         {"role": "system",
-         "content": "You are an expert in prompt creation for image generator AIs"},
+         "content": "You are an expert in prompt engineering for image generator AIs"},
         {"role": "user", "content": (
                 'Make changes in the following html code based on the folowing description'
                 'In this html code there are images with alt and figcaption tags connected to them'
                 'I want you to replace the text in alt tags with a full prompt for image generator AI'
                 'The prompt should be based on both the figcaption and the text'
                 'Do not include any other changes to the text'
-                'Return only the html code'
+                'Return only the html code but without code markdowns'
                 f'Here’s the text:{cleanedHtml}'
             )}
     ]
 )
 finalHtml = imagePromptResponse.choices[0].message.content.strip()
-finalHtml = finalHtml[8:-3]
 
 
 with open('artykul.html', 'w', encoding='utf-8') as file:
