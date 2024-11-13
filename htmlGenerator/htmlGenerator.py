825ba78adb380e8af2244d440aa0dd10e3b38712
jshra <Jshutyra@gmail.com>
2024-11-13 17:19:42 +0100

    Minor changes to calls and prompts

--- htmlGenerator/htmlGenerator.py
+++ htmlGenerator/htmlGenerator.py
@@ -3,16 +3,17 @@ client = OpenAI(api_key = '')
 
 with open('text.txt', 'r', encoding='utf-8') as file:
     text = file.read()
+model = "gpt-4o-mini"
 
 htmlResponse = client.chat.completions.create(
-    model="gpt-4o-mini",
+    model= model,
     messages=[
         {"role": "system",
          "content": "You are an expert in HTML and webpage design"},
         {"role": "user", "content": (
                 "Convert the following text into a structured HTML format. "
                 "Use HTML tags only for structure."
-                "Add images where suitable by inserting <img> tag with attribute src='image_placeholder.jpg'"
+                "Add images where suitable by inserting figures with <img> tag with attribute src='image_placeholder.jpg'"
                 "Include at least two images"
                 "Add captions to those images using suitable HTML tag "
                 "Return only the code to go between <body> and </body>"
@@ -27,13 +28,13 @@ htmlResponse = client.chat.completions.create(
 cleanedHtml = htmlResponse.choices[0].message.content.strip()
 
 imagePromptResponse = client.chat.completions.create(
-    model="gpt-4o-mini",
+    model= model,
     messages=[
         {"role": "system",
          "content": "You are an expert in prompt engineering for image generator AIs"},
         {"role": "user", "content": (
                 'Make changes in the following html code based on the folowing description'
-                'In this html code there are images with alt and figcaption tags connected to them'
+                'In this html code there are images in figures with alt and figcaption tags connected to them'
                 'I want you to replace the text in alt tags with a full prompt for image generator AI'
                 'The prompt should be based on both the figcaption and the text'
                 'Do not include any other changes to the text'
